8ae6eb1e8b41f694c7cbe3635871793bcab63106
serifer <110091342+serifer@users.noreply.github.com>
2023-01-29 18:09:10 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+(function (){
+    var NetworkSecurityConfig=Java.use("android.security.net.config.NetworkSecurityConfig");
+    // @ts-ignore
+    NetworkSecurityConfig.getDefaultBuilder.implementation=function (info){
+        var oriVer=info.targetSdkVersion.value;
+        info.targetSdkVersion.value=20;
+        var ret=this.getDefaultBuilder(info);
+        info.targetSdkVersion.value=oriVer;
+        return ret;
+    }
+})();
