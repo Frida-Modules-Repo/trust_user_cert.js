47d704c74ebffcdb00a825979919602ad2a4767d
serifer <110091342+serifer@users.noreply.github.com>
2023-01-29 18:12:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 (function (){
-    var NetworkSecurityConfig=Java.use("android.security.net.config.NetworkSecurityConfig");
-    // @ts-ignore
-    NetworkSecurityConfig.getDefaultBuilder.implementation=function (info){
-        var oriVer=info.targetSdkVersion.value;
-        info.targetSdkVersion.value=20;
-        var ret=this.getDefaultBuilder(info);
-        info.targetSdkVersion.value=oriVer;
-        return ret;
-    }
+    Java.perform(function () {
+        var NetworkSecurityConfig=Java.use("android.security.net.config.NetworkSecurityConfig");
+        // @ts-ignore
+        NetworkSecurityConfig.getDefaultBuilder.implementation=function (info){
+            var oriVer=info.targetSdkVersion.value;
+            info.targetSdkVersion.value=20;
+            var ret=this.getDefaultBuilder(info);
+            info.targetSdkVersion.value=oriVer;
+            return ret;
+        }
+    });
 })();
